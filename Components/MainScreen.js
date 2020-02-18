0fbc2513bb916b7715e2e8f4a0f1b2bc0fe1f57f
kmhayeon <kmhayeon@naver.com>
2020-02-19 04:18:34 +0900

    tab

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Icon } from 'native-base';
 import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 import HomeTab from './AppTabNavigator/HomeTab'
 import SearchTab from './AppTabNavigator/SearchTab'
 import AddMediaTab from './AppTabNavigator/AddMediaTab'
@@ -10,12 +10,31 @@ import LikesTab from './AppTabNavigator/LikesTab'
 import ProfileTab from './AppTabNavigator/ProfileTab'
 
 // 하단 탭 네비게이터 생성
-const AppTabNavigator = createBottomTabNavigator({
-    HomeTab: { screen: HomeTab },
-    SearchTab: { screen: SearchTab },
-    AddMediaTab: { screen: AddMediaTab },
-    LikesTab: { screen: LikesTab },
-    ProfileTab: { screen: ProfileTab }
+const AppTabNavigator = createMaterialTopTabNavigator({
+    HomeTab:{ screen: HomeTab },
+    Search:{ screen: SearchTab },
+    AddMedia:{ screen: AddMediaTab },
+    Likes:{ screen: LikesTab },
+    Profile:{ screen: ProfileTab }
+},  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+        style: {
+            ...Platform.select({
+                ios:{
+                    backgroundColor:'white',
+                }
+            })
+        },
+        iconStyle: { height: 100 },
+        activeTintColor: '#000',
+        inactiveTintColor: '#d1cece',
+        upperCaseLabel: false,
+        showLabel: false,
+        showIcon: true,
+    }
 });
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
